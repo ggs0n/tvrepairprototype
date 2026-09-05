2b0832087805264c5271fe6464814d3546651101
ggs0n <adibafifisaadon@gmail.com>
2026-09-05 23:40:19 +0800

    Add payment summary and quotation ui change

--- TVRepair.Web/src/pages/QuotationCustomer.jsx
+++ TVRepair.Web/src/pages/QuotationCustomer.jsx
@@ -27,26 +27,86 @@ export default function QuotationCustomer ({orders})
               const response = await paymentsummary.json();
               setPaymentSummary(response)
             }
+        }
 
+        async function Decline()
+        {
+            const paymentsummary = await fetch('http://localhost:5070/api/payment/DeclinePayment', {
+                method : "POST",
+                type : {
+                    type : "application/json"
+                },
+                headers : {
+                  "Content-Type" : "application/json"
+                },
+                body : JSON.stringify ({
+                    orders
+                })
+            })
 
+            if (paymentsummary.ok)
+            {
+              const response = await paymentsummary.json();
+              setPaymentSummary(response)
+            }
         }
 
         return (
-            <div className="border border-1">
-                <div className="grid-cols-2 align-middle justify-between flex">
-                    <div>
-                    <img className="item-center align-middle" src="../src/assets/boxempty.png" width={300}></img>
+            <div className="justify-center grid-cols-2 flex gap-5 p-10 rounded-xl bg-gradient from-50% bg-green-800 to bg-black">
+                <div className="p-5 border-1 bg-white">
+                    <div className="mb-6 shadow-lg p-4">
+                    <h1 className="text-2xl font-bold">Device Information</h1>
+                    <h1>Brand : {orders.brand}</h1>
+                    <h1>Issue Description : {orders.issueDescription}</h1>
+                    <h1>Accessories :</h1>
                     </div>
-                    <div>
 
-                    <div className="text-center py-10 w-100">
-                    <h1>Quotation Detail</h1>
-                    <p>Issue : {orders.quotationDesc}</p>
-                    <p>Amount : {orders.quotationAmount}</p>
+                    <div className="mb-6 shadow-lg p-4">
+                    <h1 className="text-2xl font-bold">Technician Information</h1>
+                    <h1>Name : {orders.technicianId}</h1>
+                    <h1>Rating : 5/5</h1>
+                    <h1>Date Pickup : {orders.createdDate}</h1>
+                    <h1>Date Quotation : { orders.quotationDate}</h1>
+                    <h1>Service Area : {orders.area}</h1>
                     </div>
-                    </div>
-                    <button className="bg-green-700" onClick={Pay}>Accept & Pay</button>
                 </div>
+
+                <div className="border border-1 p-5 bg-white">
+                    <div>
+                    <div className="py-5 align-middle">
+                    <h1 className="text-3xl">Quotation Summary</h1>
+                    <p className="mb-4">Diagnostic Result : {orders.quotationDesc}</p>
+
+                    <table className="border border-1 m-0 p-4">
+                        <thead className="border border-2 bg-gray-400">
+                            <tr>
+                                <th className="px-4 py-3">Item / Part</th>
+                                <th className="px-4 py-3">Description</th>
+                                <th className="px-4 py-3 text-right">Unit Price (RM)</th>
+                                <th className="px-4 py-3 text-right">Total (RM)</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                                <tr key={orders.id} className="hover:bg-gray-50">
+                                <td className="px-4 py-3 font-medium">{orders.brand}</td>
+                                <td className="px-4 py-3 text-gray-600">{orders.quotationDesc}</td>
+                                <td className="px-4 py-3 text-center">{orders.quotationAmount}</td>
+                                <td className="px-4 py-3 text-right">{orders.quotationAmount.toFixed(2)}
+                                </td>
+                                </tr>
+                        </tbody>
+                        <p>Total : {orders.quotationAmount}</p>
+                    </table>
+                    
+                    </div>
+                    <div className="flex gap-4">
+                    <button className="bg-green-700 cursor-pointer w-100 text-white p-2 items-center align-middle" onClick={Pay}>Accept & Pay</button>
+                    <button className="bg-red-700 cursor-pointer w-100 text-white p-2 items-center align-middle" onClick={Decline}>Decline</button>
+                    </div>
+                    </div>
+                </div>
+
                 
             </div>
         )
